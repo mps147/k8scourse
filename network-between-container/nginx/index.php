3f15d441e33b16df3433d68110b9c29772eb39ab
Marcelo Pereira <marcelopereiraifsp@gmail.com>
2023-06-16 18:12:26 -0300

    first commit

---
+++ network-between-container/nginx/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+// define('HOST', 'network_course');
+// define('DB_NAME', 'DB_DOCKER');
+// define('USER', 'root');
+// define('PASS', 'root');
+
+// $con = new PDO('mysql:host=' . HOST . ';dbname=' . DB_NAME, USER, PASS);
+
+// $curl = curl_init();
+
+// curl_setopt($curl, CURLOPT_URL, "https://randomuser.me/api");
+
+echo "Hello world";
